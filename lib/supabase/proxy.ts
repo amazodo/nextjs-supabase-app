3229c67fe95d1e289a56853576d098dc281f7e6f
amazodo <amazodo@gmail.com>
2026-08-19 22:57:01 +0900

    supabase 초기 세팅

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -47,11 +47,13 @@ export async function updateSession(request: NextRequest) {
   const { data } = await supabase.auth.getClaims();
   const user = data?.claims;
 
-  if (
+if (
     request.nextUrl.pathname !== "/" &&
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    !request.nextUrl.pathname.startsWith("/auth") &&
+    request.nextUrl.pathname !== "/instruments" &&
+    !request.nextUrl.pathname.startsWith("/instruments/")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
